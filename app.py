ef3d9aeb2f0ba8c3c1838814a5c257c39ebb853c
mona <zhangruomeng2@gmail.com>
2016-11-21 18:19:30 -0500

    blueprints version

--- app.py
+++ app.py
@@ -1,28 +1,23 @@
 # all the imports
 import os
 import time
-from sqlite3 import dbapi2 as sqlite3
 from hashlib import md5
 from datetime import datetime
 from flask import Flask, request, session, url_for, redirect, \
      render_template, abort, g, flash, _app_ctx_stack, Response
 from werkzeug import check_password_hash, generate_password_hash
 from functools import wraps
+from flask_sqlalchemy import SQLAlchemy
+import sqlite3
 
-import logging
-logging.basicConfig(level=logging.INFO)
 # create the application object
 app = Flask(__name__)
 
 # config
-app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'groupwise.db'),
-    DEBUG=True,
-    SECRET_KEY='development key',
-    USERNAME='admin',
-    PASSWORD='default'
-))
+app.config.from_object('config.DevelopmentConfig')
 
+# create the sqlalchemy object
+db = SQLAlchemy(app)
 
 # login required decorator
 def login_required(f):
@@ -221,8 +216,8 @@ def logout():
 
 
 # connect to database
-def connect_db():
-    return sqlite3.connect(app.database)
+# def connect_db():
+#     return sqlite3.connect(app.database)
 
 
 # add some filters to jinja
@@ -231,4 +226,4 @@ app.jinja_env.filters['gravatar'] = gravatar_url
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
